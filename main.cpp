66f0382ea77806a8ece4149ba5e4009c13769687
Core2DuoEver <75671497+Core2DuoEver@users.noreply.github.com>
2025-11-27 23:50:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,11 @@ int main() {
 	std::future<int> future_min = std::async(std::launch::async, &array::min, &arr);
 	std::future<int> future_sum = std::async(std::launch::async, &array::sum, &arr);
 
-	std::cout << "Max: " << future_max.get() << "\n";
-	std::cout << "Min: " << future_min.get() << "\n";
-	std::cout << "Sum: " << future_sum.get() << "\n";
+	int int_future_max = future_max.get();
+	int int_future_min = future_min.get();
+	int int_future_sum = future_sum.get(); //This one needs to be done for everyone to pass
+
+	std::cout << "Max: " << int_future_max << "\n";
+	std::cout << "Min: " << int_future_min << "\n";
+	std::cout << "Sum: " << int_future_min << "\n";
 }
